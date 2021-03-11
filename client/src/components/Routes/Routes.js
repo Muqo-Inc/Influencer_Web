eecc5694f4ceef538fa0072839a6e89fde9feb31
parikrut <4krutikparikh>
2021-03-10 21:30:34 -0500

    included privacy policy

--- client/src/components/Routes/Routes.js
+++ client/src/components/Routes/Routes.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { FormPage } from "../layouts/formPage";
 import { GetStartedPage } from "../layouts/GetStartedPage";
-
+import PrivacyPolicy from '../Privacy-Policy/Privacy_Policy'
 export const Routes = () => {
   return (
     <section>
       <Switch>
         <Route path="/form" component={FormPage} />
         <Route path="/get_started" component={GetStartedPage} />
+        <Route path="/privacy" component={PrivacyPolicy} />
       </Switch>
     </section>
   );
